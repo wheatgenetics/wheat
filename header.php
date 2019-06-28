a0519ece5123d750d776693770c0c36a1756083a
josiahaltschuler <josiah.altschuler@gmail.com>
2019-06-28 13:10:35 -0500

    added a wrapper for flexbox to the title and description

--- header.php
+++ header.php
@@ -26,22 +26,20 @@
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<?php the_custom_logo(); ?>
+			<div class="site-branding__text">
+				<?php if ( is_front_page() && is_home() ) : ?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php else : ?>
+					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 				<?php
-			else :
+				endif;
+				$wheat_description = get_bloginfo( 'description', 'display' );
+				if ( $wheat_description || is_customize_preview() ) :
 				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$wheat_description = get_bloginfo( 'description', 'display' );
-			if ( $wheat_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $wheat_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
+					<p class="site-description"><?php echo $wheat_description; /* WPCS: xss ok. */ ?></p>
+				<?php endif; ?>
+			</div><!-- .site-branding__text -->
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
