3d3c8aef94c981888f996d2bf67621937ba79cf3
josiahaltschuler <josiah.altschuler@gmail.com>
2019-07-03 14:06:37 -0500

    added plus and minus symbols to dropdown menu items

--- js/navigation.js
+++ js/navigation.js
@@ -12,23 +12,25 @@
 
 		// Add dropdown toggle that displays child menu items.
 		var dropdownToggle = $( '<button />', { 'class': 'dropdown-toggle', 'aria-expanded': false })
-			.append( wheatScreenReaderText.icon )
+			.append( $( '<span />', { 'class': 'dropdown-symbol', text: '+' }) )
 			.append( $( '<span />', { 'class': 'screen-reader-text', text: wheatScreenReaderText.expand }) );
 
 		container.find( '.menu-item-has-children > a, .page_item_has_children > a' ).after( dropdownToggle );
 
 		// Set the active submenu dropdown toggle button initial state.
-		container.find( '.current-menu-ancestor > button' )
-			.addClass( 'toggled-on' )
-			.attr( 'aria-expanded', 'true' )
-			.find( '.screen-reader-text' )
-			.text( wheatScreenReaderText.collapse );
+		// container.find( '.current-menu-ancestor > button' )
+		// 	.addClass( 'toggled-on' )
+		// 	.attr( 'aria-expanded', 'true' )
+		// 	.find( '.screen-reader-text' )
+		// 	.text( wheatScreenReaderText.collapse );
 		// Set the active submenu initial state.
-		container.find( '.current-menu-ancestor > .sub-menu' ).addClass( 'toggled-on' );
+		// container.find( '.current-menu-ancestor > .sub-menu' ).addClass( 'toggled-on' );
 
 		container.find( '.dropdown-toggle' ).click( function( e ) {
 			var _this = $( this ),
 				screenReaderSpan = _this.find( '.screen-reader-text' );
+				dropdownSymbol = _this.find( '.dropdown-symbol' );
+				dropdownSymbol.text( dropdownSymbol.text() === '-' ? '+' : '-' );
 
 			e.preventDefault();
 			_this.toggleClass( 'toggled-on' );
