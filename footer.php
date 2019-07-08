fb085b50b656dd80f0da3137ce312c51730a3918
josiahaltschuler <josiah.altschuler@gmail.com>
2019-07-08 16:12:56 -0500

    removed site-info div from footer

--- footer.php
+++ footer.php
@@ -21,19 +21,6 @@
 			) );
 			?>
 		</nav>
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wheat' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'wheat' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'wheat' ), 'wheat', '<a href="http://wheatgenetics.k-state.edu">Poland Lab, Department of Plant Pathology, Kansas State University</a>' );
-				?>
-		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
