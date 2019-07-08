b220110027e105238a77ab03241f0e39a32a9641
josiahaltschuler <josiah.altschuler@gmail.com>
2019-07-08 17:19:25 -0500

    removed entry header from home page

--- front-page.php
+++ front-page.php
@@ -1,11 +1,6 @@
 <?php
 /**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
+ * The template for displaying the front page
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
@@ -21,9 +16,47 @@ get_header();
 		<?php
 		while ( have_posts() ) :
 			the_post();
+            ?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-			get_template_part( 'template-parts/content', 'page' );
-		endwhile; // End of the loop.
+
+	<?php wheat_post_thumbnail(); ?>
+
+	<div class="entry-content">
+		<?php
+		the_content();
+
+		wp_link_pages( array(
+			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wheat' ),
+			'after'  => '</div>',
+		) );
+		?>
+	</div><!-- .entry-content -->
+
+	<?php if ( get_edit_post_link() ) : ?>
+		<footer class="entry-footer">
+			<?php
+			edit_post_link(
+				sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Edit <span class="screen-reader-text">%s</span>', 'wheat' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					get_the_title()
+				),
+				'<span class="edit-link">',
+				'</span>'
+			);
+			?>
+		</footer><!-- .entry-footer -->
+	<?php endif; ?>
+</article><!-- #post-<?php the_ID(); ?> -->
+		<?php endwhile; // End of the loop.
 		?>
 
 		</main><!-- #main -->
