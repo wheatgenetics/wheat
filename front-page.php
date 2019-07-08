051b180293902290def5dcaf16d0b755dfe85257
josiahaltschuler <josiah.altschuler@gmail.com>
2019-07-08 17:25:50 -0500

    removed extra edit and page links from home page content

--- front-page.php
+++ front-page.php
@@ -16,48 +16,16 @@ get_header();
 		<?php
 		while ( have_posts() ) :
 			the_post();
-            ?>
+        ?>
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            	<?php wheat_post_thumbnail(); ?>
 
+                <div class="entry-content">
+                    <?php the_content(); ?>
+                </div><!-- .entry-content -->
 
-	<?php wheat_post_thumbnail(); ?>
-
-	<div class="entry-content">
-		<?php
-		the_content();
-
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wheat' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div><!-- .entry-content -->
-
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'wheat' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					get_the_title()
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
-</article><!-- #post-<?php the_ID(); ?> -->
-		<?php endwhile; // End of the loop.
-		?>
+            </article><!-- #post-<?php the_ID(); ?> -->
+		<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
