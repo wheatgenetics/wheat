3d5cdae685354a26a255c2569a64bbaf7f1b34d5
josiahaltschuler <josiah.altschuler@gmail.com>
2019-08-19 16:35:08 -0500

    added research areas

--- front-page.php
+++ front-page.php
@@ -9,24 +9,42 @@
 
 get_header();
 ?>
+	<div id="research-areas">
+		<?php 
+		$args = array(
+			'post_type'=> 'research_areas'
+		);              
+
+		$researchAreas = new WP_Query($args);
+
+		if ($researchAreas->have_posts()) {
+			while ($researchAreas->have_posts()) {
+				$researchAreas->the_post();
+				echo '<div class="research-box">';
+				echo '<p><i style="font-size: 3em;" class="fas fa-dna"></i></p>';
+				the_title();
+				echo '</div>';
+			}
+		}
+		?>
+	</div>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-        
-		<?php
-		while ( have_posts() ) :
-			the_post();
-        ?>
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <?php wheat_post_thumbnail(); ?>
-                
-                <div class="entry-content">
-                    <?php the_content(); ?>
-                </div><!-- .entry-content -->
+			<?php
+			while ( have_posts() ) :
+				the_post();
+			?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<?php wheat_post_thumbnail(); ?>
+					
+					<div class="entry-content">
+						<?php the_content(); ?>
+					</div><!-- .entry-content -->
 
-            </article><!-- #post-<?php the_ID(); ?> -->
-		<?php endwhile; // End of the loop. ?>
+				</article><!-- #post-<?php the_ID(); ?> -->
+			<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
