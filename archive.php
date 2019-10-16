754826462375c224e4d90208a25519753ab5843c
josiahaltschuler <josiah.altschuler@gmail.com>
2019-10-16 16:40:07 -0500

    archive pages now display title in a purple header bar

--- archive.php
+++ archive.php
@@ -9,44 +9,41 @@
 
 get_header();
 ?>
+	<div class="container">
+		<div id="page-title">
+			<?php post_type_archive_title(); ?>
+		</div>
+		
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main">
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
+			<?php if ( have_posts() ) : ?>
 				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+				/* Start the Loop */
+				while ( have_posts() ) :
+					the_post();
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+					/*
+					* Include the Post-Type-specific template for the content.
+					* If you want to override this in a child theme, then include a file
+					* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+					*/
+					get_template_part( 'template-parts/content', get_post_type() );
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+				endwhile;
 
-			endwhile;
+				the_posts_navigation();
 
-			the_posts_navigation();
+			else :
 
-		else :
+				get_template_part( 'template-parts/content', 'none' );
 
-			get_template_part( 'template-parts/content', 'none' );
+			endif;
+			?>
 
-		endif;
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+			</main><!-- #main -->
+		</div><!-- #primary -->
+	</div>
 
 <?php
 get_sidebar();
