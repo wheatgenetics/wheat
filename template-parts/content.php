9c1a682b1184a744ef4261a480cfb3ae415838cd
Josiah Altschuler <josiah.altschuler@gmail.com>
2019-10-21 11:44:13 -0500

    updated the structure and styles of archive pages

--- template-parts/content.php
+++ template-parts/content.php
@@ -9,13 +9,23 @@
 
 ?>
 
+<?php
+$authors = get_field('authors');
+$journal = get_field('journal');
+$publication_date = get_field('publication_date');
+$link = get_field('link');
+$journal_date = [];
+?>
+
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
+		<?php the_post_thumbnail('thumbnail'); ?>
+	
 		<?php
 		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
+			the_title( '<h3 class="entry-title">', '</h3>' );
 		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			the_title( '<h3 class="entry-title"><a href="' . $link . '" rel="bookmark" target="_blank">', '</a></h3>' );
 		endif;
 
 		if ( 'post' === get_post_type() ) :
@@ -29,28 +39,28 @@
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php wheat_post_thumbnail(); ?>
-
 	<div class="entry-content">
-		<?php
-		the_content( sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'wheat' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		) );
+		<?php													
+		if (!empty($authors)) {
+			echo "<p>" . $authors . "</p>";
+		}
+
+		if (!empty($journal)) {
+			$journal_date[] = $journal;
+		}
+		
+		if (!empty($publication_date)) {
+			$journal_date[] = $publication_date;
+		}
+
+		echo "<p class='gray' style='text-transform:uppercase;'>" . implode(' | ', $journal_date) . "</p>";
 
 		wp_link_pages( array(
 			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wheat' ),
 			'after'  => '</div>',
 		) );
 		?>
+		
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
