b2da5ed259ad1cbaad2b2830717c779ef00b9a87
Josiah Altschuler <josiah.altschuler@gmail.com>
2019-10-22 14:41:50 -0500

    added container wrapper to the rest of the templates

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
 get_header();
 ?>
 
+<div class="container">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
@@ -53,7 +54,7 @@ get_header();
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
+</div>
 <?php
 get_sidebar();
 get_footer();
