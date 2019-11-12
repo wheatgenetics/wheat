4b6742e15fdbcf19869c879ca03a678fba1f0530
Josiah Altschuler <josiah.altschuler@gmail.com>
2019-11-12 13:45:57 -0600

    added jquery to underline the current project in sidebar

--- functions.php
+++ functions.php
@@ -191,6 +191,7 @@ function wheat_scripts() {
 		
 	// ) );
 	wp_enqueue_script( 'wheat-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'underline-current-project', get_template_directory_uri() . '/js/underline-current-project.js', array('jquery'), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
---
+++ js/underline-current-project.js
@@ -0,0 +1,12 @@
+/**
+ * File underline-current-project.js.
+ *
+ * Underlines current project in sidebar pf Project pages
+ *
+ */
+( function( $ ) {
+  $(document).ready(function(){
+    var title = $('h3.entry-title').html();
+    $(`a:contains(${title})`).parent().css('border-bottom', '1px solid #FDC345');
+  });
+} )( jQuery );
