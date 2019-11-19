50fa9d3b3ac77a49cddbbeba505e88c6edf4c333
josiahaltschuler <josiah.altschuler@gmail.com>
2019-11-19 16:26:12 -0600

    removed unneeded featured image from front page template

--- front-page.php
+++ front-page.php
@@ -39,9 +39,7 @@ get_header('front');
 			while ( have_posts() ) :
 				the_post();
 			?>
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<?php wheat_post_thumbnail(); ?>
-					
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>					
 					<div class="entry-content">
 						<?php the_content(); ?>
 					</div><!-- .entry-content -->
