7a6ca283d16724a0ad9d26ca674a8f511d66e9eb
josiahaltschuler <josiah.altschuler@gmail.com>
2019-11-19 16:34:31 -0600

    removed extra spaces in code

--- front-page.php
+++ front-page.php
@@ -39,7 +39,7 @@ get_header('front');
 			while ( have_posts() ) :
 				the_post();
 			?>
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>					
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>				
 					<div class="entry-content">
 						<?php the_content(); ?>
 					</div><!-- .entry-content -->
