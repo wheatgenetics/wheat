9cb8bd9215c67abbc28260efc470a6e80f504de7
josiahaltschuler <josiah.altschuler@gmail.com>
2019-11-19 13:52:51 -0600

    cleaned up the content-none template a bit. removed search boxes.

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -33,18 +33,12 @@
 
 		elseif ( is_search() ) :
 			?>
-
 			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'wheat' ); ?></p>
 			<?php
-			get_search_form();
-
 		else :
 			?>
-
-			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'wheat' ); ?></p>
+			<p><?php esc_html_e( 'There is no content here yet. Please try again at a later date.', 'wheat' ); ?></p>
 			<?php
-			get_search_form();
-
 		endif;
 		?>
 	</div><!-- .page-content -->
