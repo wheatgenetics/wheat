c83918093b8949e9e6d19fdb5844ddf60f270b0c
josiahaltschuler <josiah.altschuler@gmail.com>
2020-03-10 10:23:31 -0500

    removed featured image from single template and removed content column flex container

--- single.php
+++ single.php
@@ -40,92 +40,90 @@ $slider = get_field('slider');
 						<?php if (!empty($slider)) {
 							echo $slider;
 						} ?>
-						<?php the_post_thumbnail('thumbnail'); ?>
 
-						<div class="header-content-wrapper">
-							<header class="entry-header">
-								<?php
+            <header class="entry-header">
+              <?php
 
-								the_title( '<h3 class="entry-title">', '</h3>' );
+              the_title( '<h3 class="entry-title">', '</h3>' );
 
-								echo "<p class='gray' style='text-transform:uppercase;'>" . $publication_date . "</p>";
+              echo "<p class='gray' style='text-transform:uppercase;'>" . $publication_date . "</p>";
 
-								if ( 'post' === get_post_type() ) :
-									?>
-									<div class="entry-meta">
-										<?php
-										wheat_posted_on();
-										wheat_posted_by();
-										?>
-									</div><!-- .entry-meta -->
-								<?php endif; ?>
-							</header><!-- .entry-header -->
+              if ( 'post' === get_post_type() ) :
+                ?>
+                <div class="entry-meta">
+                  <?php
+                  wheat_posted_on();
+                  wheat_posted_by();
+                  ?>
+                </div><!-- .entry-meta -->
+              <?php endif; ?>
+            </header><!-- .entry-header -->
 
-							<div class="entry-content">
-								<?php
-								// the_post_thumbnail('medium', ['class' => 'alignleft']);
-								// the_post_thumbnail_caption();
-									
-								if (!empty($authors)) {
-									echo "<p>" . $authors . "</p>";
-								}
-								
-								if (!empty($publication_date)) {
-									$journal_date[] = $publication_date;
-								}
+            <div class="entry-content">
+              <?php
+              // the_post_thumbnail('thumbnail');
+              // the_post_thumbnail('medium', ['class' => 'alignleft']);
+              // the_post_thumbnail_caption();
+                
+              if (!empty($authors)) {
+                echo "<p>" . $authors . "</p>";
+              }
+              
+              if (!empty($publication_date)) {
+                $journal_date[] = $publication_date;
+              }
 
-								if (!empty($title_position)) {
-									echo $title_position;
-								}
+              if (!empty($title_position)) {
+                echo $title_position;
+              }
 
-								if ($post_type == 'people') {
-									echo '<p>' . $bio . '</p>';
-									echo $office_location;
-									echo '<br><a href="mailto:' . $email . '" target="_blank">' . $email . '</a>';
-									echo '<br><a href="tel:' . $phone . '" target="_blank">' . $phone . '</a>';
-								}
+              if ($post_type == 'people') {
+                echo '<p>' . $bio . '</p>';
+                echo $office_location;
+                echo '<br><a href="mailto:' . $email . '" target="_blank">' . $email . '</a>';
+                echo '<br><a href="tel:' . $phone . '" target="_blank">' . $phone . '</a>';
+              }
 
-								the_content(
-									sprintf(
-										wp_kses(
-											/* translators: %s: Name of current post. Only visible to screen readers */
-											__( '', 'wheat' ),
-											array(
-												'span' => array(
-													'class' => array(),
-												),
-											)
-										),
-										get_the_title()
-									)
-								);
+              the_content(
+                sprintf(
+                  wp_kses(
+                    /* translators: %s: Name of current post. Only visible to screen readers */
+                    __( '', 'wheat' ),
+                    array(
+                      'span' => array(
+                        'class' => array(),
+                      ),
+                    )
+                  ),
+                  get_the_title()
+                )
+              );
 
-								wp_link_pages( array(
-									'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wheat' ),
-									'after'  => '</div>',
-								) );
-								?>
-								
-								<!-- <br>
+              wp_link_pages( array(
+                'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wheat' ),
+                'after'  => '</div>',
+              ) );
+              ?>
+              
+              <!-- <br>
 
-								<hr class="dotted">
+              <hr class="dotted">
 
-								<br> -->
+              <br> -->
 
-								<?php if ($post_type == 'news') { ?>
-									<a href="<?php echo $link; ?>" class="dark-gray-button" target="_blank">Go to original story</a>
-								<?php } ?>
+              <?php if ($post_type == 'news') { ?>
+                <a href="<?php echo $link; ?>" class="dark-gray-button" target="_blank">Go to original story</a>
+              <?php } ?>
 
-								<?php if ($post_type == 'projects' && !empty($project_website)) { ?>
-									<br>
-									<a href="<?php echo $project_website; ?>" class="dark-gray-button" target="_blank">Visit project website</a>
-								<?php } ?>
-							</div><!-- .entry-content -->
+              <?php if ($post_type == 'projects' && !empty($project_website)) { ?>
+                <br>
+                <a href="<?php echo $project_website; ?>" class="dark-gray-button" target="_blank">Visit project website</a>
+              <?php } ?>
+            </div><!-- .entry-content -->
 
-							<footer class="entry-footer">
-								<?php wheat_entry_footer(); ?>
-							</footer><!-- .entry-footer -->
-						</div>
+            <footer class="entry-footer">
+              <?php wheat_entry_footer(); ?>
+            </footer><!-- .entry-footer -->
 					</article><!-- #post-<?php the_ID(); ?> -->
 
 					<?php
