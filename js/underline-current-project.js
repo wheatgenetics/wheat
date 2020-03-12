29c480f4487db88fec869381d6f52bd101c43456
josiahaltschuler <josiah.altschuler@gmail.com>
2020-03-12 11:25:32 -0500

    added styles to current link via js

--- js/underline-current-project.js
+++ js/underline-current-project.js
@@ -1,12 +1,13 @@
 /**
  * File underline-current-project.js.
  *
- * Underlines current project in sidebar pf Project pages
+ * Underlines current project in sidebar of Project pages
  *
  */
 ( function( $ ) {
   $(document).ready(function(){
     var title = $('h3.entry-title').html();
-    $(`.widget a:contains(${title})`).parent().css('border-bottom', '1px solid #FDC345');
+    $(`.widget a:contains(${title})`).parent().css('border-bottom', '1px solid #FDC345').css('font-weight', 'bold');
+    $(`.widget a:contains(${title})`).css('color', '#472979');
   });
 } )( jQuery );
