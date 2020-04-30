f16ea620be9d84458775829102a6bcdd249a54e5
josiahaltschuler <josiah.altschuler@gmail.com>
2020-04-30 09:54:21 -0500

    minor code formatting change from tabs to spaces

--- template-parts/content.php
+++ template-parts/content.php
@@ -20,61 +20,61 @@ $journal_date = [];
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php the_post_thumbnail('thumbnail'); ?>
+  <?php the_post_thumbnail('thumbnail'); ?>
 
-	<div class="header-content-wrapper">
-		<header class="entry-header">
-			<?php
-			if ($post_type == 'publications') {
-				the_title( '<h3 class="entry-title"><a href="' . $link . '" rel="bookmark" target="_blank">', '</a></h3>' );
-			} else {
-				if (is_singular()) {
-					the_title( '<h3 class="entry-title">', '</h3>' );
-				} else {
-					the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
-				}
-			}
+  <div class="header-content-wrapper">
+    <header class="entry-header">
+      <?php
+      if ($post_type == 'publications') {
+        the_title( '<h3 class="entry-title"><a href="' . $link . '" rel="bookmark" target="_blank">', '</a></h3>' );
+      } else {
+        if (is_singular()) {
+          the_title( '<h3 class="entry-title">', '</h3>' );
+        } else {
+          the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
+        }
+      }
 
-			if ( 'post' === get_post_type() ) :
-				?>
-				<div class="entry-meta">
-					<?php
-					wheat_posted_on();
-					wheat_posted_by();
-					?>
-				</div><!-- .entry-meta -->
-			<?php endif; ?>
-		</header><!-- .entry-header -->
+      if ( 'post' === get_post_type() ) :
+        ?>
+        <div class="entry-meta">
+          <?php
+          wheat_posted_on();
+          wheat_posted_by();
+          ?>
+        </div><!-- .entry-meta -->
+      <?php endif; ?>
+    </header><!-- .entry-header -->
 
-		<div class="entry-content">
-			<?php													
-			if (!empty($authors)) {
-				echo "<p>" . $authors . "</p>";
-			}
+    <div class="entry-content">
+      <?php													
+      if (!empty($authors)) {
+        echo "<p>" . $authors . "</p>";
+      }
 
-			if (!empty($journal)) {
-				$journal_date[] = $journal;
-			}
-			
-			if (!empty($publication_date)) {
-				$journal_date[] = $publication_date;
-			}
+      if (!empty($journal)) {
+        $journal_date[] = $journal;
+      }
+      
+      if (!empty($publication_date)) {
+        $journal_date[] = $publication_date;
+      }
 
-			the_excerpt();
+      the_excerpt();
 
-			if ($post_type == 'publications' || $post_type == 'news') {
-				echo "<p class='gray' style='text-transform:uppercase;'>" . implode('&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;', $journal_date) . "</p>";
-			}
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wheat' ),
-				'after'  => '</div>',
-			) );
-			?>
-			
-		</div><!-- .entry-content -->
-	</div>
-	
-	<footer class="entry-footer">
-		<?php wheat_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+      if ($post_type == 'publications' || $post_type == 'news') {
+        echo "<p class='gray' style='text-transform:uppercase;'>" . implode('&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;', $journal_date) . "</p>";
+      }
+      wp_link_pages( array(
+        'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wheat' ),
+        'after'  => '</div>',
+      ) );
+      ?>
+      
+    </div><!-- .entry-content -->
+  </div>
+  
+  <footer class="entry-footer">
+    <?php wheat_entry_footer(); ?>
+  </footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
