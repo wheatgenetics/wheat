ea80cd64c942b154436d96394eb265fc567ef318
josiahaltschuler <josiah.altschuler@gmail.com>
2020-05-01 09:34:17 -0500

    formatted code spacing

--- header.php
+++ header.php
@@ -13,71 +13,71 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 
 <!-- <input type="checkbox" id="menuToggler" class="input-toggler" />
 <label for="menuToggler" class="menu-toggler">
-	<span class="menu-toggler__line"></span>
-	<span class="menu-toggler__line"></span>
-	<span class="menu-toggler__line"></span>
+  <span class="menu-toggler__line"></span>
+  <span class="menu-toggler__line"></span>
+  <span class="menu-toggler__line"></span>
 </label> -->
 
 <!-- <aside class="sidebar"> -->
-	<?php
-	// wp_nav_menu( array(
-	// 	'theme_location' => 'menu-1',
-	// 	'menu_id'        => 'primary-menu',
-	// ) );
-	?>
+  <?php
+  // wp_nav_menu( array(
+  // 	'theme_location' => 'menu-1',
+  // 	'menu_id'        => 'primary-menu',
+  // ) );
+  ?>
 <!-- </aside> -->
-	
+  
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'wheat' ); ?></a>
+  <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'wheat' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="container">
-			<div class="site-branding">
-				<?php the_custom_logo(); ?>
-				<div class="site-branding__text">
-					<?php if ( is_front_page() && is_home() ) : ?>
-						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php else : ?>
-						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-					<?php
-					endif;
-					$wheat_description = get_bloginfo( 'description', 'display' );
-					if ( $wheat_description || is_customize_preview() ) :
-					?>
-						<p class="site-description"><?php echo $wheat_description; /* WPCS: xss ok. */ ?></p>
-					<?php endif; ?>
-				</div><!-- .site-branding__text -->
-			</div><!-- .site-branding -->
+  <header id="masthead" class="site-header">
+    <div class="container">
+      <div class="site-branding">
+        <?php the_custom_logo(); ?>
+        <div class="site-branding__text">
+          <?php if ( is_front_page() && is_home() ) : ?>
+            <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+          <?php else : ?>
+            <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+          <?php
+          endif;
+          $wheat_description = get_bloginfo( 'description', 'display' );
+          if ( $wheat_description || is_customize_preview() ) :
+          ?>
+            <p class="site-description"><?php echo $wheat_description; /* WPCS: xss ok. */ ?></p>
+          <?php endif; ?>
+        </div><!-- .site-branding__text -->
+      </div><!-- .site-branding -->
 
-			<nav id="site-navigation" class="main-navigation">
-				<?php
-				wp_nav_menu( array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				) );
-				?>
-			</nav><!-- #site-navigation -->
+      <nav id="site-navigation" class="main-navigation">
+        <?php
+        wp_nav_menu( array(
+          'theme_location' => 'menu-1',
+          'menu_id'        => 'primary-menu',
+        ) );
+        ?>
+      </nav><!-- #site-navigation -->
 
-			<nav class="social-menu">
-				<?php				
-				wp_nav_menu( array(
-					'theme_location' => 'social'
-				) );
-				get_search_form();
-				?>
-			</nav>
-		</div>
-	</header><!-- #masthead -->
+      <nav class="social-menu">
+        <?php				
+        wp_nav_menu( array(
+          'theme_location' => 'social'
+        ) );
+        get_search_form();
+        ?>
+      </nav>
+    </div>
+  </header><!-- #masthead -->
 
-	<div id="content" class="site-content">
+  <div id="content" class="site-content">
