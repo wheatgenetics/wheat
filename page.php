b8da73153ccf175da47e97599ec2bc80e8c98f93
josiahaltschuler <josiah.altschuler@gmail.com>
2020-05-01 10:34:16 -0500

    changed code format from tabs to spaces

--- page.php
+++ page.php
@@ -15,31 +15,31 @@
 get_header();
 ?>
 
-	<div class="container">
-		<?php get_sidebar(); ?>
+  <div class="container">
+    <?php get_sidebar(); ?>
 
-		<div id="primary" class="content-area">
-			<main id="main" class="site-main">
-				<h3 id="page-title">
-					<?php the_title(); ?>
-				</h3>
+    <div id="primary" class="content-area">
+      <main id="main" class="site-main">
+        <h3 id="page-title">
+          <?php the_title(); ?>
+        </h3>
 
-				<?php
-				while ( have_posts() ) :
-					the_post();
+        <?php
+        while ( have_posts() ) :
+          the_post();
 
-					get_template_part( 'template-parts/content', 'page' );
+          get_template_part( 'template-parts/content', 'page' );
 
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
+          // If comments are open or we have at least one comment, load up the comment template.
+          if ( comments_open() || get_comments_number() ) :
+            comments_template();
+          endif;
 
-				endwhile; // End of the loop.
-				?>
-			</main><!-- #main -->
-		</div><!-- #primary -->
-	</div>
+        endwhile; // End of the loop.
+        ?>
+      </main><!-- #main -->
+    </div><!-- #primary -->
+  </div>
 
 <?php
 get_footer();
