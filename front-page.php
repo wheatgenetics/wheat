c636244118c7bf51daad93f187e49bcd4ccded52
josiahaltschuler <josiah.altschuler@gmail.com>
2020-05-11 17:07:57 -0500

    removed conditional to show research areas on home page

--- front-page.php
+++ front-page.php
@@ -9,47 +9,25 @@
 
 get_header('front');
 ?>
-	<?php 
-	$args = array(
-		'post_type'=> 'research_areas'
-	);              
 
-	$researchAreas = new WP_Query($args);
+<div id="primary" class="content-area">
+  <main id="main" class="site-main">
 
-	if ($researchAreas->have_posts()) {
-		echo '<div id="research-areas">';
-		
-		while ($researchAreas->have_posts()) {
-			$researchAreas->the_post();
-			echo '<a class="research-box">';
-			echo '<p><i style="font-size: 3em;" class="fas fa-dna"></i></p>';
-			the_title();
-			echo '</a>';
-		}
+    <?php
+    while ( have_posts() ) :
+      the_post();
+    ?>
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        
+        <div class="entry-content">
+          <?php the_content(); ?>
+        </div><!-- .entry-content -->
 
-		echo '</div>';
-	}
-	?>
-	
+      </article><!-- #post-<?php the_ID(); ?> -->
+    <?php endwhile; // End of the loop. ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-			<?php
-			while ( have_posts() ) :
-				the_post();
-			?>
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					
-					<div class="entry-content">
-						<?php the_content(); ?>
-					</div><!-- .entry-content -->
-
-				</article><!-- #post-<?php the_ID(); ?> -->
-			<?php endwhile; // End of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+  </main><!-- #main -->
+</div><!-- #primary -->
 
 <?php
 get_footer('front');
